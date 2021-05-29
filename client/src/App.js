1dc097b79f3861fb2a1b5b6212117d152aac38fe
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2021-05-29 15:47:17 +0530

    Frontend Updated :pizza:

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,68 @@
+import {useState} from "react"
+import { useQuery, gql ,useMutation } from '@apollo/client';
 import './App.css';
 
-function App() {
+const GET_ALL_USERS = gql`
+query{
+  users{
+    name
+    age
+  }
+}
+`;
+
+const ADD_USER = gql`
+mutation addUser($name: String!,$age:Int!) {
+  addUser(name: $name,age:$age) {
+    id
+  }
+}
+`;
+
+const App = () =>{
+  const [name,setName] = useState("")
+  const [age,setAge] = useState(0)
+  const { loading, error, data,refetch  } = useQuery(GET_ALL_USERS);
+  const [addUser] = useMutation(ADD_USER);
+  if (loading){
+    return (
+      <div className="App">
+        <header className="main-header">
+          Loading
+        </header>
+        </div>
+    )
+  }
+  if (error){
+    return(
+      <div className="App">
+      <header className="main-header">
+        Error 
+      </header>
+      </div>
+    )
+  }
+
+  const handleAddUser = ()=>{
+    addUser({ variables: { name, age } });
+    refetch();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="main-header">
        ReactJS + GraphQL +Apollo
       </header>
+      <input type="text" placeholder="Enter Name" onChange={e=>setName(e.target.value)} required/>
+      <input type="number" placeholder="Enter Age" onChange={e=>setAge(parseInt(e.target.value,10))} required/>
+      <button onClick={handleAddUser}>Add User</button>
+      <hr />
+      <header className="secondary">
+        Users
+      </header>
+      <ol className="users">
+        {data?.users && data?.users.map((u)=><li>{u?.name}</li>)}
+      </ol>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloClient, InMemoryCache  } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+  <App />
+</ApolloProvider>,
   document.getElementById('root')
 );
 
